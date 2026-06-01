7b4fa748e940ff969f9e4761031b51aaef4f9f68
lendev4 <lendev4@proton.me>
2026-06-01 13:49:12 +0200

    Change site URL and base path in config
    Updated site URL and base path for deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lendev4.github.io/pagels-kindergarten2/',
-  base: 'pagels-kindergarten2',
+  site: 'https://pagels-kindergarten.de'
 });
